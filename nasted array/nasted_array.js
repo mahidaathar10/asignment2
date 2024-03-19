7554e7dffa87bf0c51f003abc3f836c401021f07
mahidaathar10 <160022747+mahidaathar10@users.noreply.github.com>
2024-03-19 05:03:32 +0500

    Add files via upload

---
+++ nasted array/nasted_array.js
@@ -0,0 +1,24 @@
+"use strict";
+const smartphone = [
+    {
+        smartPhone: "tecno spark 20",
+        storage: "128gb",
+        camera: "3.5 mega pixel  with popup camera",
+        screen: "full HD screen",
+        prize: 27000,
+        colour: "blue",
+    },
+    {
+        smartphone: "ifinix note 10 pro",
+        prize: 30000,
+        spacks: true,
+        model: null,
+        storage: "128gb",
+    }
+];
+console.log(smartphone); //now we are going to print both objects
+//assign variable to array of each object
+const smartphone_1 = smartphone[0];
+console.log("first object of array in smartphone is:", smartphone_1); //this variable going to print first object
+const smartphone_2 = smartphone[1];
+console.log(" second object of array in smartphone is:", smartphone_2); //this variable going to print second object
