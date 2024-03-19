0437dfe8e3c82de0330ff9c6d22931db713271b3
mahidaathar10 <160022747+mahidaathar10@users.noreply.github.com>
2024-03-19 05:04:19 +0500

    Add files via upload

---
+++ object asignment/object_asignment.ts
@@ -0,0 +1,19 @@
+let student={
+  name:"mahida",
+  lastName:"athar",
+  age:20,
+  qualification:"intermediate",
+  siblings:2,
+}
+  console.log(student.name);
+  console.log(student.lastName);
+  console.log(student.age);
+  console.log(student.qualification);
+  console.log(student.siblings);
+  console.log(student.name,student.lastName,student.age,student.qualification,student.siblings)
+console.log(student.name,student.qualification);
+
+  
+  
+
+
